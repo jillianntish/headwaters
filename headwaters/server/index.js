6b4832f63c90b8a5fecf2f4b193c07a77740769c
Marc West <marcdwest@gmail.com>
2019-12-26 13:04:16 -0600

    routes, ejs, expressLayouts

--- headwaters/server/index.js
+++ headwaters/server/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const path = require('path');
 
+const { users } = require('../routes/users');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/users', users);
+
+const PORT = process.env.PORT || 8080;
 const CLIENT_PATH = path.join(__dirname, '../client/dist/');
 
 app.use(express.static(CLIENT_PATH));
 
-// //ejs routing?
-// app.get('/', );
-
 app.listen(PORT, () => {
   console.log(`Listening on :${PORT} 🛸`);
-})
+});
