84db6c1f1958915e43938f202823b0e25b389022
Marc West <marcdwest@gmail.com>
2019-12-26 14:49:48 -0600

    remove ejs

--- headwaters/server/index.js
+++ headwaters/server/index.js
@@ -1,18 +1,8 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
 
 const path = require('path');
 
-const { users } = require('../routes/users');
-
 const app = express();
-
-// EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// Routes
-app.use('/users', users);
 
 const PORT = process.env.PORT || 8080;
 const CLIENT_PATH = path.join(__dirname, '../client/dist/');
