b11ef98d1b8a73c8127b5220cc5eb96b32e04d55
addupe <autumndupin@gmail.com>
2019-12-27 11:20:19 -0600

    add css loader to webpack / remove old code

--- headwaters/webpack.config.js
+++ headwaters/webpack.config.js
@@ -1,19 +1,3 @@
-// module.exports = {
-//   // Use the src/index.js file as entry point to bundle it
-//   // If the src/index.js file imports other JavaScript files, bundle them as well
-//   entry: './client/src/index.jsx',
-//   // The bundled source code files shall result in a bundle.js file in the /dist folder
-//   output: {
-//     path: `${__dirname}/dist`,
-//     publicPath: '/',
-//     filename: 'bundle.js',
-//   },
-//   // The /dist folder will be used to serve our application to the browser
-//   devServer: {
-//     contentBase: './client/dist',
-//   },
-// };
-
 const path = require('path');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
@@ -32,6 +16,10 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
