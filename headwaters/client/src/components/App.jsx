b7ae8dcabfda33c10dd517517b1deac72674fb5f
Joy <greeniejoy@gmail.com>
2019-12-27 15:17:09 -0600

    added navBar component

--- headwaters/client/src/components/App.jsx
+++ headwaters/client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Calendar from './Calendar.jsx';
 import Login from './auth/Login.jsx';
 import Register from './auth/Register.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Calendar />
         <Login />
         <Register />
