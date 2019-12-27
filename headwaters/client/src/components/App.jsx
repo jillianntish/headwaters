b5da02e64629648a6f34d1cf102777a2f7730fbb
addupe <autumndupin@gmail.com>
2019-12-26 20:54:48 -0600

    main app component

---
+++ headwaters/client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Calendar from './Calendar.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Calendar />
+    );
+  }
+}
+
+export default App;
