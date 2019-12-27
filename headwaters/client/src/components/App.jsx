9068d9f9fb4148356224e7113d76a50a4da5ba10
Marc West <marcdwest@gmail.com>
2019-12-27 15:49:46 -0600

    land on 'login', changeView, implement changeView

--- headwaters/client/src/components/App.jsx
+++ headwaters/client/src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Calendar from './Calendar.jsx';
 import Login from './auth/Login.jsx';
 import Register from './auth/Register.jsx';
 
@@ -7,15 +6,46 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'login',
     };
+
+    this.changeView = this.changeView.bind(this);
+  }
+
+  changeView(option) {
+    this.setState({
+      view: option,
+    });
   }
 
   render() {
+    const { view } = this.state;
+
     return (
       <div>
-        <Calendar />
-        <Login />
-        <Register />
+        <div className="nav">
+          <span className="logo">
+            {' '}
+            <h1>Headwaters</h1>
+          </span>
+
+          <span
+            className={view === 'login' ? 'nav-selected' : 'nav-unselected'}
+            onClick={() => this.changeView('login')}
+          >
+            <h3>Log In</h3>
+          </span>
+          <span
+            className={view === 'register' ? 'nav-selected' : 'nav-unselected'}
+            onClick={() => this.changeView('register')}
+          >
+            <h3>Register</h3>
+          </span>
+        </div>
+
+        <div className="main">
+          {view === 'login' ? <Login /> : <Register />}
+        </div>
       </div>
     );
   }
