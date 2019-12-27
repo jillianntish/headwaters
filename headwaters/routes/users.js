5174e8c0dd5a93efbd12b799894d55d89361ded0
Marc West <marcdwest@gmail.com>
2019-12-26 21:17:53 -0600

    set up test auth

--- headwaters/routes/users.js
+++ headwaters/routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 
-const users = express.Router();
+const router = express.Router();
 
-// Login
-users.get('/login', (req, res) => res.send('login'));
+const { check, validationResult } = require('express-validator');
 
-// Register
-users.get('/register', (req, res) => res.send('register'));
+// @Route --> POST api/users
+// @Desc --> Register a user
+// @Access --> Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed');
+  },
+);
 
-module.exports.users = users;
+module.exports = router;
