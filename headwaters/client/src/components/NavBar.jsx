5fc9415e938d7e587f577972d78696541ea41455
Joy <greeniejoy@gmail.com>
2019-12-27 16:32:55 -0600

    seperated dropdown

--- headwaters/client/src/components/NavBar.jsx
+++ headwaters/client/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import DropDown from './DropDown.jsx';
 
 
 const useStyles = makeStyles(theme => ({
@@ -24,20 +24,13 @@ const NavBar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-            <select>
-              <option value="calender">Calender</option>
-              <option value="journal">Journal</option>
-              <option selected value="pillbox">Pill Box</option>
-            </select>
-          </IconButton>
+          <DropDown />
           <Typography variant="h6" color="inherit">
             Headwaters
           </Typography>
         </Toolbar>
-      </AppBar >
-    </div >
+      </AppBar>
+    </div>
   )
 }
 
