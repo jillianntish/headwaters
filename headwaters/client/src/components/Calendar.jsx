0c8fedeb7c09521aa9265252b6a2e85d466beb9e
addupe <autumndupin@gmail.com>
2019-12-27 21:05:35 -0600

    add time and duration features to sample events

--- headwaters/client/src/components/Calendar.jsx
+++ headwaters/client/src/components/Calendar.jsx
@@ -2,31 +2,40 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import timeGridPlugin from "@fullcalendar/timegrid";
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 import '../styles/calendar.css';
 
-const Calendar = (props) => {
-  return (
-    <div className="cal-font">
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        { title: 'birthday', start: '2019-12-28T14:30:00' },
+        { title: 'surfing', start: '2019-12-29', end: '2020-01-01' },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="cal-font">
       <div className="calendar-top" />
       <div className="calendar">
         <FullCalendar
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           header={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth, timeGridWeek, timeGridDay, listWeek"
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek',
           }}
-          events={[
-            { title: "event 1", date: "2019-12-27" },
-            { title: "event 2", date: "2019-12-28" }
-          ]}
+          events={this.state.events}
         />
       </div>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Calendar;
