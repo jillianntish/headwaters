7c6330814a99da165eefee005868a4a1a9b21e1e
Joy <greeniejoy@gmail.com>
2019-12-30 11:01:36 -0600

    updated app component

--- headwaters/client/src/components/App.jsx
+++ headwaters/client/src/components/App.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import Calendar from './Calendar.jsx';
 import Login from './auth/Login.jsx';
 import Register from './auth/Register.jsx';
-import NavBar from './NavBar.jsx';
-import Journal from './Journal.jsx';
+import Calendar from './Calendar.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'login',
     };
+
+    this.changeView = this.changeView.bind(this);
+  }
+
+  changeView(option) {
+    this.setState({
+      view: option,
+    });
   }
 
   render() {
+    const { view } = this.state;
+
     return (
       <div>
-        <NavBar />
+        <div className="nav">
+          <span className="logo">
+            {' '}
+            <h1>Headwaters</h1>
+          </span>
+
+          <span
+            className={view === 'login' ? 'nav-selected' : 'nav-unselected'}
+            onClick={() => this.changeView('login')}
+          >
+            <h3>Log In</h3>
+          </span>
+          <span
+            className={view === 'register' ? 'nav-selected' : 'nav-unselected'}
+            onClick={() => this.changeView('register')}
+          >
+            <h3>Register</h3>
+          </span>
+        </div>
+
+        <div className="main">
+          {view === 'login' ? <Login /> : <Register />}
+        </div>
         <Calendar />
-        <Login />
-        <Register />
-        <Journal />
       </div>
     );
   }
