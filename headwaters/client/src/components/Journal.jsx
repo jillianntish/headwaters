a25b17cf4e8c52e909d37dfc25f407b3c9a39988
Joy <greeniejoy@gmail.com>
2019-12-29 18:52:29 -0600

    fixed input type for exercise

--- headwaters/client/src/components/Journal.jsx
+++ headwaters/client/src/components/Journal.jsx
@@ -4,66 +4,75 @@ class Journal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      journal: "",
+      feeling: "",
 
     };
   }
 
+  const onChange = e => e.target.value
+});
 
-  render() {
-    return (
-      <div>
-        <div className="form-container">
-          <h1>
-            Journal <span className="text-primary"></span>
-          </h1>
-          <form>
-            <div>
-              <textarea rows="10" cols="50">
-                Today's journal entry...
+const onSubmit = e => {
+  e.preventDefault();
+  console.log('journal submit');
+};
+
+render() {
+  return (
+    <div>
+      <div className="form-container">
+        <h1>
+          Journal <span className="text-primary"></span>
+        </h1>
+        <form>
+          <div>
+            <textarea rows="10" cols="50">
+              Today's journal entry...
             </textarea>
+          </div>
+          <div>
+            <label htmlFor="feelings">How are you feeling today?</label>
+            <select>
+              <option value="happiness">happiness</option>
+              <option value="anger">anger</option>
+              <option value="sadness">sadness</option>
+              <option value="love">love</option>
+              <option value="fear">fear</option>
+              <option value="depression">depression</option>
+              <option value="disgust">disgust</option>
+              <option value="surprise">surprise</option>
+              <option value="neutral">neutral</option>
+              <option value="anxiety">anxiety</option>
+              <option value="contempt">contempt</option>
+              <option value="pride">pride</option>
+              <option value="shame">shame</option>
+              <option value="envy">envy</option>
+            </select>
+            <div>
+              <label htmlFor="water">How much water did you drink today?</label>
+              <input type="water" name="water" />
             </div>
             <div>
-              <label htmlFor="feelings">How are you feeling today?</label>
-              <select>
-                <option value="happiness">happiness</option>
-                <option value="anger">anger</option>
-                <option value="sadness">sadness</option>
-                <option value="love">love</option>
-                <option value="fear">fear</option>
-                <option value="depression">depression</option>
-                <option value="disgust">disgust</option>
-                <option value="surprise">surprise</option>
-                <option value="neutral">neutral</option>
-                <option value="anxiety">anxiety</option>
-                <option value="contempt">contempt</option>
-                <option value="pride">pride</option>
-                <option value="shame">shame</option>
-                <option value="envy">envy</option>
-              </select>
-              <div>
-                <label htmlFor="water">How much water did you drink today?</label>
-                <input type="water" name="water" />
-              </div>
-              <div>
-                <label htmlFor="sleep">How much sleep did you get?</label>
-                <input type="sleep" name="sleep" />
-              </div>
-              <div>
-                <label htmlFor="sleep">How much exercise did you get?</label>
-                <input type="sleep" name="sleep" />
-              </div>
-              <div>
-                <textarea rows="3" cols="50">
-                  What did you eat?
-            </textarea>
-              </div>
+              <label htmlFor="sleep">How much sleep did you get?</label>
+              <input type="sleep" name="sleep" />
             </div>
-            <input type="submit" value="submit" />
-          </form>
-        </div>
+            <div>
+              <label htmlFor="exercise">How much exercise did you get?</label>
+              <input type="exercise" name="exercise" />
+            </div>
+            <div>
+              <textarea rows="3" cols="50">
+                What did you eat?
+            </textarea>
+            </div>
+          </div>
+          <input type="submit" value="submit" />
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 
