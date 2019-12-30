1abe472f20f7d0864ba2c87417c9e5453de85096
addupe <autumndupin@gmail.com>
2019-12-30 11:12:49 -0600

    add journal component as route

--- headwaters/client/src/components/App.jsx
+++ headwaters/client/src/components/App.jsx
@@ -4,6 +4,7 @@ import history from '../utils/history';
 // import Login from './auth/Login.jsx';
 // import Register from './auth/Register.jsx';
 import Calendar from './Calendar.jsx';
+import Journal from './Journal.jsx';
 import NavBar from './NavBar.jsx';
 
 class App extends React.Component {
@@ -58,6 +59,7 @@ class App extends React.Component {
           <Switch>
             <Route path="/" exact />
             <Route path="/calendar" component={Calendar} />{' '}
+            <Route path="/journal" component={Journal} />{' '}
           </Switch>
         </Router>
       </div>
