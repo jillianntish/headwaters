7694b3ee962688084981e553a23b803af702645d
Joy <greeniejoy@gmail.com>
2019-12-31 15:46:57 -0600

    moved userId to end of route

--- headwaters/routes/journal.js
+++ headwaters/routes/journal.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/users/:userId/api/journal', (req, res) => res.send('get journal from db'));
+router.get('/api/journal/:userId', (req, res) => res.send('get journal from db'));
 
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-router.post('/users/:userId/api/journal', (req, res) => res.send('send journal input to server'));
+router.post('/api/journal/:userId', (req, res) => res.send('send journal input to server'));
 
 module.exports = router;
 
