fbff8925e08af0b1b6601a6bd074c5fe3b59e202
Marc West <marcdwest@gmail.com>
2019-12-30 19:34:27 -0600

    cleanup

--- headwaters/client/src/components/App.jsx
+++ headwaters/client/src/components/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from '../utils/history';
-// import Login from './auth/Login.jsx';
-// import Register from './auth/Register.jsx';
 import Calendar from './Calendar.jsx';
 import Journal from './Journal.jsx';
 import NavBar from './NavBar.jsx';
@@ -13,8 +11,6 @@ class App extends React.Component {
   }
 
   render() {
-    // const { view } = this.state;
-
     return (
       <div className="App">
         <Router history={history}>
@@ -25,26 +21,6 @@ class App extends React.Component {
             </span>
             <NavBar />
           </header>
-          {/* <div className="nav">
-            <span
-              className={view === 'login' ? 'nav-selected' : 'nav-unselected'}
-              onClick={() => this.changeView('login')}
-            >
-              <h3>Log In</h3>
-            </span>
-            <span
-              className={
-                view === 'register' ? 'nav-selected' : 'nav-unselected'
-              }
-              onClick={() => this.changeView('register')}
-            >
-              <h3>Register</h3>
-            </span>
-          </div>
-
-          <div className="main">
-            {view === 'login' ? <Login /> : <Register />}
-          </div> */}
           <Switch>
             <Route path="/" exact />
             <Route path="/calendar" component={Calendar} />{' '}
