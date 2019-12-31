bad3824a34407c0cd4cb6d91896672e9b0a536a2
Joy <greeniejoy@gmail.com>
2019-12-31 14:10:26 -0600

    made journal route

--- headwaters/routes/journal.js
+++ headwaters/routes/journal.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/journal', (req, res) => res.send('get journal from db'));
+
+// @Route --> POST api/pillbpx
+// @Desc --> post pillbox input data to sever/db
+router.post('/journal', (req, res) => res.send('send journal input to server'));
+
+module.exports = router;
+
 module.exports = router;
