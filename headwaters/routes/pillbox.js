bd655bd0597ab7e160ff83a99a30581ee4dbca27
Joy <greeniejoy@gmail.com>
2019-12-31 14:09:25 -0600

    fixed pillbox route

--- headwaters/routes/pillbox.js
+++ headwaters/routes/pillbox.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // @Route --> GET api/pillbox
 // @Desc --> Get pillbox from database
 
-router.get('/pillbox', (req, res) => res.send('get logged in user'));
+router.get('/pillbox', (req, res) => res.send('get pillbox from db'));
 
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-router.post('/pillbox', (req, res) => res.send('log in a user'));
+router.post('/pillbox', (req, res) => res.send('send pillbox input to server'));
 
 module.exports = router;
