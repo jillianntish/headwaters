7562c2a1151631def171246350ba0ee2e18192b1
addupe <autumndupin@gmail.com>
2019-12-31 15:16:02 -0600

    get request handler / skeleton for post

--- headwaters/routes/calendar.js
+++ headwaters/routes/calendar.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const { getUserEvents } = require('../server/db/connection');
 
-const router = express.Router();
+const calendarRouter = express.Router();
 
-router.get('/', (req, res) => res.send());
+calendarRouter.get('/:userId/events', (req, res) => {
+  const { userId } = req.params;
 
-router.post('/', (req, res) => res.send());
+  getUserEvents(userId)
+    .then(userRows => {
+      if (userRows.length) {
+        res.send(userRows);
+      }
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      res.sendStatus(404);
+    });
+});
 
-module.exports = router;
+calendarRouter.post('/:userId/events', (req, res) => {
+  // use db helper to post a new event that matches user id
+  // send back to client 201 status code
+  //  501 if not completed
+});
+
+module.exports = calendarRouter;
