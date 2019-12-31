75adabd2bf2aa46f4d7a2414de12553ab0c9c6d7
Joy <greeniejoy@gmail.com>
2019-12-31 14:16:25 -0600

    updated routes to include userId

--- headwaters/routes/journal.js
+++ headwaters/routes/journal.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/journal', (req, res) => res.send('get journal from db'));
+router.get('/users/:userId/api/journal', (req, res) => res.send('get journal from db'));
 
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-router.post('/journal', (req, res) => res.send('send journal input to server'));
+router.post('/users/:userId/api/journal', (req, res) => res.send('send journal input to server'));
 
 module.exports = router;
 
--- headwaters/routes/pillbox.js
+++ headwaters/routes/pillbox.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // @Route --> GET api/pillbox
 // @Desc --> Get pillbox from database
 
-router.get('/pillbox', (req, res) => res.send('get pillbox from db'));
+router.get('/users/:userId/api/pillbox', (req, res) => res.send('get pillbox from db'));
 
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-router.post('/pillbox', (req, res) => res.send('send pillbox input to server'));
+router.post('/users/:userId/api/pillbox', (req, res) => res.send('send pillbox input to server'));
 
 module.exports = router;
