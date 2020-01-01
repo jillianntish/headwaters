a99513cf9b2d7da5839197514bb5e734494457b2
Marc West <marcdwest@gmail.com>
2020-01-01 13:44:25 -0600

    display calendar and event form side by side

--- headwaters/client/src/components/Calendar/Calendar.jsx
+++ headwaters/client/src/components/Calendar/Calendar.jsx
@@ -3,6 +3,7 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import { Col, Container, Row } from 'reactstrap';
 import NewEvent from './NewEvent.jsx';
 import EventOptions from './EventOptions.jsx';
 import { useAuth0 } from '../../react-auth0-spa.jsx';
@@ -13,45 +14,49 @@ import '../../styles/calendar.css';
 const Calendar = () => {
   const { user } = useAuth0();
 
-  const [events, setEvents] = useState([{
-    user,
-    id: '',
-    title: '',
-    start: '',
-    extendedProps: {
-      practictioner: '',
-      location: '',
+  const [events, setEvents] = useState([
+    {
+      user,
+      id: '',
+      title: '',
+      start: '',
+      extendedProps: {
+        practictioner: '',
+        location: '',
+      },
     },
-  }]);
+  ]);
 
   useEffect(() => {
     // axios helper
-    setEvents([{
-      title: 'Immunization for Away Team',
-      start: '2019-12-31T15:00:00',
-      extendedProps: {
-        practicioner: 'Dr. Crusher',
-        location: 'Starship Enterprise',
+    setEvents([
+      {
+        title: 'Immunization for Away Team',
+        start: '2019-12-31T15:00:00',
+        extendedProps: {
+          practicioner: 'Dr. Crusher',
+          location: 'Starship Enterprise',
+        },
       },
-    }]);
+    ]);
   }, []);
 
-
   const [clickedDate, setClickedDate] = useState([]);
-  const [clickedEvent, setClickedEvent] = useState([{
-    title: '',
-    state: '',
-    practicioner: '',
-    location: '',
-  }]);
-
+  const [clickedEvent, setClickedEvent] = useState([
+    {
+      title: '',
+      state: '',
+      practicioner: '',
+      location: '',
+    },
+  ]);
 
   const [showEventForm, setShowEventForm] = useState(false);
   const [showEventOptions, setShowEventOptions] = useState(false);
 
   // const handleTimeConversion = () => {}
 
-  const handleDateClick = (arg) => {
+  const handleDateClick = arg => {
     setClickedDate([arg.date]);
     setShowEventForm(true);
     if (showEventOptions) {
@@ -59,14 +64,15 @@ const Calendar = () => {
     }
   };
 
-  const eventClick = (info) => {
-    
-    setClickedEvent([{
-      title: info.event.title,
-      start: info.event.start.toString(),
-      practicioner: info.event.extendedProps.practicioner,
-      location: info.event.extendedProps.location,
-    }]);
+  const eventClick = info => {
+    setClickedEvent([
+      {
+        title: info.event.title,
+        start: info.event.start.toString(),
+        practicioner: info.event.extendedProps.practicioner,
+        location: info.event.extendedProps.location,
+      },
+    ]);
 
     setShowEventOptions(true);
     if (showEventForm) {
@@ -75,28 +81,39 @@ const Calendar = () => {
   };
 
   return (
-    <div className="cal-font">
-      <div className="calendar-top" />
-      <div className="calendar">
-        <FullCalendar
-          defaultView="dayGridMonth"
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          header={{
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek',
-          }}
-          selectable
-          events={events}
-          dateClick={handleDateClick}
-          eventClick={eventClick}
-        />
+    <Container>
+      <div className="cal-font">
+        <div className="calendar-top" />
+        <Row>
+          <Col xs="6">
+            <div className="calendar">
+              <FullCalendar
+                defaultView="dayGridMonth"
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                header={{
+                  left: 'prev,next today',
+                  center: 'title',
+                  right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek',
+                }}
+                selectable
+                events={events}
+                dateClick={handleDateClick}
+                eventClick={eventClick}
+              />
+            </div>
+          </Col>
+          <Col xs="6">
+            {showEventForm && (
+              <NewEvent className="calendar" date={clickedDate} />
+            )}
+          </Col>
+          <Col xs="6">
+            {showEventOptions && <EventOptions event={clickedEvent} />}
+          </Col>
+        </Row>
       </div>
-      {showEventForm ? <NewEvent date={clickedDate} /> : <div />}
-      {showEventOptions ? <EventOptions event={clickedEvent} /> : <div />}
-    </div>
+    </Container>
   );
 };
-
 
 export default Calendar;
