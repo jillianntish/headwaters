94c48e6ce32c0ab3eb5f288a16f078f88b2931d5
addupe <autumndupin@gmail.com>
2019-12-31 20:21:29 -0600

    code cleanup in routing

--- headwaters/routes/calendar.js
+++ headwaters/routes/calendar.js
@@ -17,9 +17,6 @@ calendarRouter.get('/:userId/events', (req, res) => {
 });
 
 calendarRouter.post('/:userId/events', (req, res) => {
-  // use db helper to post a new event that matches user id
-  // send back to client 201 status code
-  //  501 if not completed
   const newEventObj = req.body[0];
 
   insertUserEvent(newEventObj)
