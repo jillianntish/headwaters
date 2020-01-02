375f31d7afd07ac7299bf3314c7d28cd99af7cf2
Joy <greeniejoy@gmail.com>
2020-01-02 10:52:10 -0600

    added to post and get request

--- headwaters/routes/pillbox.js
+++ headwaters/routes/pillbox.js
@@ -1,14 +1,26 @@
 const express = require('express');
 
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 // @Route --> GET api/pillbox
 // @Desc --> Get pillbox from database
 
-router.get('/api/pillbox/:userId', (req, res) => res.send('get pillbox from db'));
-
+router.get('/api/pillbox/1', (req, res) => {
+  //helper function to get data(medication info) from db to post to user
+  // getMeds()
+  // .then(response => {
+  //   res.send('get pillbox from db', response);
+  // })
+  // .catch(error => {
+  //   console.log("uh oh,couldn't get data from db");
+  // })
+});
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-router.post('/api/pillbox/:userId', (req, res) => res.send('send pillbox input to server'));
-
+router.post('/api/pillbox/:userId', (req, res) => {
+  console.log("getting data from client", req.body);
+  //helper function to save data to db
+  res.send('send pillbox input to server');
+}
 module.exports = router;
