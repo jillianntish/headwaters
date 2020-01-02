2fdaffcad3b72396bbed3ee6a1c6c38347ab5386
Marc West <marcdwest@gmail.com>
2020-01-01 21:36:09 -0600

    remove bad comments

--- headwaters/routes/journal.js
+++ headwaters/routes/journal.js
@@ -4,8 +4,6 @@ const journalRouter = express.Router();
 
 journalRouter.get('/journal/:userId/entries', (req, res) => res.send('get journal from db'));
 
-// @Route --> POST api/pillbpx
-// @Desc --> post pillbox input data to sever/db
 journalRouter.post('/journal/:userId/entries', (req, res) => res.send('send journal input to server'));
 
 module.exports = journalRouter;
