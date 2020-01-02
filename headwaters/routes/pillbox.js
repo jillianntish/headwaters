6ad8f6a37da9e46862cec49d1d7bda173c90d07b
Marc West <marcdwest@gmail.com>
2020-01-01 21:34:22 -0600

    remove /

--- headwaters/routes/pillbox.js
+++ headwaters/routes/pillbox.js
@@ -4,10 +4,10 @@ const pillboxRouter = express.Router();
 
 // @Route --> GET api/pillbox
 // @Desc --> Get pillbox from database
-pillboxRouter.get('/pillbox/:userId/', (req, res) => res.send('get pillbox from db'));
+pillboxRouter.get('/pillbox/:userId', (req, res) => res.send('get pillbox from db'));
 
 // @Route --> POST api/pillbpx
 // @Desc --> post pillbox input data to sever/db
-pillboxRouter.post('/pillbox/:userId/', (req, res) => res.send('send pillbox input to server'));
+pillboxRouter.post('/pillbox/:userId', (req, res) => res.send('send pillbox input to server'));
 
 module.exports = pillboxRouter;
