41f1d1d2a3a569d8e07935fcb11f75dd20495f02
Joy <greeniejoy@gmail.com>
2020-01-02 11:09:45 -0600

    made get and post request for pillbox

--- headwaters/client/src/utils/helpers.js
+++ headwaters/client/src/utils/helpers.js
@@ -5,13 +5,13 @@ export const validateEmail = async email => {
   return await axios.get('/api/auth', { params: { email } }).then(res => res.data);
 };
 
-export const createUser = async({ nickname, email }) => {
+export const createUser = async ({ nickname, email }) => {
   return await axios.post('/api/auth', { nickname, email });
 };
 
 // axios calendar helpers
 
-export const createUserEvent = async(eventObj) => {
+export const createUserEvent = async (eventObj) => {
   return await axios.post(`/calendar/${eventObj.userId}/events`, eventObj)
     .then(res => {
       console.log(res.status);
@@ -50,3 +50,18 @@ export const handleIncomingData = incomingEvents => {
 // axios journal helpers
 
 // axios pillbox helpers
+export const addMed = async ({ med, dosage, times, notes, pic }) => {
+  return await axios.post('/:userId', { med, dosage, times, notes, pic })
+    .then((response) => {
+      console.log("posting pillbox to server", response);
+    })
+    .catch((error) => {
+      console.log("error posting pillbox info", error);
+    });
+};
+
+export const getListofMeds = async ({ med, dosage, times, notes, pic }) => {
+  return await axios.get('/:userId', { params: { med, dosage, times, notes, pic } })
+    .then(res => res.data)
+    .catch(err => console.log("error getting data from db for pillbox", err);
+};
