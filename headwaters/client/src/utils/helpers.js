8a49bd3d137b2598614a5118293010c8e274149b
addupe <autumndupin@gmail.com>
2020-01-01 22:49:16 -0600

    handling incoming data / formatting events

--- headwaters/client/src/utils/helpers.js
+++ headwaters/client/src/utils/helpers.js
@@ -12,17 +12,44 @@ export const createUser = async({ nickname, email }) => {
 // axios calendar helpers
 
 export const createUserEvent = async(eventObj) => {
-  await axios.post(`/calendar/${eventObj.userId}/events`, eventObj)
+  return await axios.post(`/calendar/${eventObj.userId}/events`, eventObj)
     .then(res => {
-      debugger;
+      console.log(res.status);
     })
     .catch(err => {
       console.error(err);
     });
 };
 
+export const handleIncomingData = incomingEvents => {
+  let formattedEvents = [];
+  const eventData = incomingEvents.map(incomingEvent => {
+    const start = incomingEvent.date_time;
+    start.replace(" ", "T");
+
+    formattedEvents = formattedEvents.concat({
+      user: incomingEvent.event_id_user,
+      id: incomingEvent.id,
+      title: incomingEvent.name,
+      start,
+      extendedProps: {
+        practictioner: incomingEvent.practicioner,
+        location: incomingEvent.location,
+        notes: incomingEvent.notes,
+        type: incomingEvent.type
+      }
+    });
+
+    return formattedEvents;
+  });
+
+  return eventData;
+};
+
 export const getUserEvents = async userId => {
-  await axios.get(`/calendar/${userId}/events`).then(res => res.data);
+  await axios.get(`/calendar/${userId}/events`).then(res => {
+    return handleIncomingData(res.data);
+  });
 };
 
 // export const editUserEvent = async({}) => {
