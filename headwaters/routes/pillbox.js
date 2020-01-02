4645f6d3718bee12d3ffe8d32256e694be776e6c
addupe <autumndupin@gmail.com>
2020-01-02 14:22:45 -0600

    working pillbox routers

--- headwaters/routes/pillbox.js
+++ headwaters/routes/pillbox.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const { addUserMedicationMaster, getUserMedications } = require('../server/db/connection');
 
 const pillboxRouter = express.Router();
 
 // @Route --> GET '/:userId'
 // @Desc --> Get a user's pillbox from database
-pillboxRouter.get('/:userId', (req, res) => res.send('get pillbox from db'));
+pillboxRouter.get('/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  getUserMedications(userId) 
+  .then(response => {
+    res.send(response);
+  })
+  .catch(err => {
+    console.error(err);
+    res.sendStatus(404);
+  })
+  
+});
 
 // @Route --> POST '/:userId'
 // @Desc --> post user's pillbox input data to database
-pillboxRouter.post('/:userId', (req, res) => res.send('send pillbox input to server'));
+pillboxRouter.post('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const newMedicationObj = req.body[0];
+
+  addUserMedicationMaster(newMedicationObj, userId)
+    .then(okResponse => {
+      debugger;
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(501);
+    })
+});
 
 module.exports = pillboxRouter;
