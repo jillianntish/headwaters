6fb5cbabc8d41eee8b5ec60eb166e3ba95626b1d
addupe <autumndupin@gmail.com>
2020-01-03 17:20:00 -0600

    clean up

--- headwaters/client/src/components/MedList.jsx
+++ headwaters/client/src/components/MedList.jsx
@@ -1,26 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {
-  Button, Form, FormGroup, Label, Input,
-} from 'reactstrap';
-
-import { useAuth0 } from '../react-auth0-spa.jsx';
+import React from 'react';
 import '../styles/event-form.css';
 import '../styles/pillbox.css';
-import getUserMedications from '../utils/helpers';
-import sample from './exampleData';
 
 const MedList = ({ medEntries }) => {
-  const { user } = useAuth0();
-
   return (
     <div>
       {
         medEntries.map(medEntry => (
           <div id="rcorners1">
             <li>Medication: {medEntry.name} </li>
-            <li> Physician: {medEntry.practitioner} </li>
-            <li> Dosage: {medEntry.dosage} </li>
+            <li>Physician: {medEntry.practitioner} </li>
+            <li>Dosage: {medEntry.dosage} </li>
             <li>Times: {medEntry.scheduled_times} </li>
             <li>Notes: {medEntry.notes} </li>
             <li>Picture:</li>
