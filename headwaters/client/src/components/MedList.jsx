aa07f351ebf21c76026e1735edd7bbe278c6e23e
Marc West <marcdwest@gmail.com>
2020-01-04 14:28:57 -0600

    convert to toast

--- headwaters/client/src/components/MedList.jsx
+++ headwaters/client/src/components/MedList.jsx
@@ -6,21 +6,19 @@ import '../styles/pillbox.css';
 const MedList = ({ medEntries }) => {
   return (
     <div>
-      {
-        medEntries.map((medEntry, index) => (
-          <Toast>
-            <ToastHeader>Rx</ToastHeader>
-            <ToastBody key={index} className="box">
-              <img src={medEntry.url} height="95" width="95" alt="" />
-              <div>{medEntry.name}</div>
-              <div>{medEntry.practitioner}</div>
-              <div>{medEntry.dosage}mg</div>
-              <div>{medEntry.scheduled_times} </div>
-              <div>{medEntry.notes} </div>
-            </ToastBody>
-          </Toast>
-        ))
-      }
+      {medEntries.map((medEntry, index) => (
+        <Toast>
+          <ToastHeader>Rx</ToastHeader>
+          <ToastBody key={index} className="box">
+            <img src={medEntry.url} height="95" width="95" alt="" />
+            <div>{medEntry.name}</div>
+            <div>{medEntry.practitioner}</div>
+            <div>{medEntry.dosage}mg</div>
+            <div>{medEntry.scheduled_times} </div>
+            <div>{medEntry.notes} </div>
+          </ToastBody>
+        </Toast>
+      ))}
     </div>
   );
 };
