fbd157a393e4afad841e21f84e43ecfadba76b74
Marc West <marcdwest@gmail.com>
2020-01-04 00:34:52 -0600

    fix props and newMed, move toggle

--- headwaters/client/src/components/toasts/PillboxToast.jsx
+++ headwaters/client/src/components/toasts/PillboxToast.jsx
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 
-const PillboxToast = ({
-  name, practitioner, dosage, scheduled_times, notes, url, show,
-}) => {
-  const [showToast, setToast] = useState(show);
-  debugger;
+const PillboxToast = ({ newMed, toggle, isOpen }) => {
+  const {
+    med, practitioner, dosage, times, notes,
+  } = newMed;
 
-  const toggle = () => {
-    setToast(!show);
-  };
   return (
-    <Toast isOpen={showToast}>
-      <ToastHeader toggle={toggle}>Event Submitted</ToastHeader>
+    <Toast isOpen={isOpen}>
+      <ToastHeader toggle={toggle}>Medication Entered</ToastHeader>
       <ToastBody>
-        <div>{name}</div>
+        <div>{med}</div>
         <div>{practitioner}</div>
         <div>{dosage}</div>
-        <div>{scheduled_times}</div>
+        <div>{times}</div>
         <div>{notes}</div>
-        <div>{url}</div>
       </ToastBody>
     </Toast>
   );
