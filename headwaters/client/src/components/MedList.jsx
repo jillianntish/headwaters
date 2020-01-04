98691a74e92273a9c9eacc41fb86e604c2fb9987
Marc West <marcdwest@gmail.com>
2020-01-04 14:27:00 -0600

    convert to toast

--- headwaters/client/src/components/MedList.jsx
+++ headwaters/client/src/components/MedList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 import '../styles/event-form.css';
 import '../styles/pillbox.css';
 
@@ -7,14 +8,17 @@ const MedList = ({ medEntries }) => {
     <div>
       {
         medEntries.map((medEntry, index) => (
-          <div key={index} className="box">
-            <img src={medEntry.url} height="95" width="95" alt="" />
-            <div>{medEntry.name}</div>
-            <div>{medEntry.practitioner}</div>
-            <div>{medEntry.dosage}mg</div>
-            <div>{medEntry.scheduled_times} </div>
-            <div>{medEntry.notes} </div>
-          </div>
+          <Toast>
+            <ToastHeader>Rx</ToastHeader>
+            <ToastBody key={index} className="box">
+              <img src={medEntry.url} height="95" width="95" alt="" />
+              <div>{medEntry.name}</div>
+              <div>{medEntry.practitioner}</div>
+              <div>{medEntry.dosage}mg</div>
+              <div>{medEntry.scheduled_times} </div>
+              <div>{medEntry.notes} </div>
+            </ToastBody>
+          </Toast>
         ))
       }
     </div>
