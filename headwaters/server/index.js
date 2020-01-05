4d6a54d071ef48de3ccd4c873b95f5c5c32d6c1b
addupe <autumndupin@gmail.com>
2020-01-05 14:32:09 -0600

    https cert

--- headwaters/server/index.js
+++ headwaters/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const path = require('path');
 const { DB_NAME } = require('./db/connection');
 const { checkEmail, newUser, findUserByEmail } = require('./db/connection');
@@ -33,10 +34,17 @@ app.post('/api/auth', async(req, res) => {
   res.json(foundUsers[0]);
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8443;
 const CLIENT_PATH = path.join(__dirname, '../client/dist/');
+// const credentials = { key: privateKey, cert: certificate };
+const privateKey = '0000';
+const certificate = '0000';
 
+const credentials = { key: privateKey, cert: certificate };
 app.use(express.static(CLIENT_PATH));
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
 
 app.listen(PORT, () => {
   console.log(`Listening on :${PORT} 🛸`);
