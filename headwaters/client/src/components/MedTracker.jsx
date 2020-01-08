091fb61ee88b28dc958ba7c6828a1ab372f07af8
Paul Stumpe <paul.andrew.stumpe@gmail.com>
2020-01-08 14:33:47 -0600

    have a timeline rendering not formatted

--- headwaters/client/src/components/MedTracker.jsx
+++ headwaters/client/src/components/MedTracker.jsx
@@ -3,6 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '../react-auth0-spa.jsx';
 import { Container } from 'reactstrap';
 import '../styles/event-form.css';
+import {
+  Timeline,
+  Events,
+  UrlButton,
+  ImageEvent,
+  TextEvent,
+  YouTubeEvent,
+} from '@merc/react-timeline';
+// import Chronology from 'react-chronos';
 
 const MedTracker = () => {
   const { user } = useAuth0();
@@ -16,6 +25,33 @@ const MedTracker = () => {
     <Container>
       <div className="med-tracker">
         <h1 style={{ color: '#1B2F44', fontWeight: 'bolder', paddingLeft: '5px', paddingTop: '10px' }}>Medicine Tracker</h1>
+        <h1>Paul Town</h1>
+        <Timeline>
+          <Events>
+            <TextEvent date="1/1/19" text="**Markdown** is *supported*" />
+
+            <ImageEvent
+              date="4/13/19"
+              text="You can embed images..."
+              src="https://res.cloudinary.com/dovoq8jou/image/upload/v1564772194/jellyfish.jpg"
+              alt="jellyfish swimming"
+              credit="Photo by [@tavi004](https://unsplash.com/@tavi004)"
+            >
+              <div>
+                <UrlButton href="https://unsplash.com/search/photos/undersea">
+                  View more undersea photos
+            </UrlButton>
+              </div>
+            </ImageEvent>
+
+            <YouTubeEvent
+              date="6/18/19"
+              id="6UnRHtwHGSE"
+              name="General Tso's Chicken recipe"
+              text="... and YouTube videos!"
+            />
+          </Events>
+        </Timeline>
       </div>
     </Container>
   );
