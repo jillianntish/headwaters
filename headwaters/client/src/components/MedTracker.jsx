279c5fdc602895204c2f8b6f2c455d70323ba3d3
Paul Stumpe <paul.andrew.stumpe@gmail.com>
2020-01-11 10:28:00 -0600

    working on displaying dynamic data further

--- headwaters/client/src/components/MedTracker.jsx
+++ headwaters/client/src/components/MedTracker.jsx
@@ -35,7 +35,6 @@ const MedTracker = () => {
     return newArr.join("/");
   }
   const toggle = () => setDropdownOpen(prevState => !prevState);
-
   //settings for the timeline
   const opts = {
     layout: "inline-evts"
